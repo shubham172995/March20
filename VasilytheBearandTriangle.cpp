d120b91de81ea1a1a81735a82b57bced27246c45
shubham <shubham.wheofficial@gmail.com>
2020-03-22 17:09:02 +0530

    add existing files

---
+++ VasilytheBearandTriangle.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	long long x, y;
+	cin>>x>>y;
+	if(x>0&&y>0){
+		cout<<"0 "<<x+y<<" "<<x+y<<" 0\n";
+	}
+	else if(x<0&&y>0){
+		cout<<x-y<<" 0 0 "<<y-x<<endl;
+	}
+	else if(x<0&&y<0){
+		cout<<x+y<<" 0 0 "<<x+y<<endl;
+	}
+	else{
+		cout<<"0 "<<y-x<<" "<<x-y<<" 0"<<endl;
+	}
+	return 0;
+}
